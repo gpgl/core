935a9823c32f2a2101533f791b415fd51579611b
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-01 22:58:59 +0000

    create database file, skip env

--- src/Database.php
+++ src/Database.php
@@ -2,92 +2,29 @@
 
 namespace gpgl\core;
 
-use Crypt_GPG;
-use Crypt_GPG_BadPassphraseException;
-
 class Database
 {
-    protected $filename;
-    protected $data;
-    protected $gpg;
-    protected $key;
-    protected $password;
+    protected $data = [];
 
-    public function __construct(string $filename = null, string $password = null, string $key = null)
+    public function getData() : array
     {
-        $this->gpg = new Crypt_GPG;
-
-        $this->key = $key;
-        $this->password = $password;
-
-        if (empty($filename)) {
-            $filename = getenv('GPGL_DB');
-        }
-
-        $this->import($filename, $password, $key);
+        return $this->data;
     }
 
-    public function index() : array
+    public function setData(array $data) : Database
     {
-        return array_keys($this->data);
+        $this->data = $data;
+        return $this;
     }
 
-    public function get(string $key) : array
+    public function get(string $key)
     {
-        return $this->data[$key] ?? [];
+        return $this->data[$key] ?? null;
     }
 
-    public function set(string $key, array $values) : bool
+    public function set(string $key, $value) : Database
     {
-        return $values === $this->data[$key] = $values;
-    }
-
-    protected function import(string $filename, string $password = null, string $key = null) : array
-    {
-        try {
-
-            // default no password needed
-            $json = $this->gpg->decryptFile($filename);
-
-        } catch (Crypt_GPG_BadPassphraseException $ex) {
-
-            if (empty($key)) {
-                $key = static::getFirstKeyIdFromEncryptedData($filename);
-            }
-
-            $json = $this->gpg
-                ->addDecryptKey($key, $password)
-                ->decryptFile($filename);
-
-        }
-
-        $this->filename = $filename;
-        $this->key = $key;
-        $this->password = $password;
-
-        return $this->data = json_decode($json, $array = true);
-    }
-
-    public function export() : bool
-    {
-        $json = json_encode($this->data);
-
-        $encrypted = $this->gpg->addEncryptKey($this->key)
-                        ->encrypt($json, $ascii = false);
-
-        return false !== file_put_contents($this->filename, $encrypted);
-    }
-
-    /**
-     * @param  string $filename Name of encrypted file.
-     * @return string The key ID of the *first* recipient.
-     */
-    public static function getFirstKeyIdFromEncryptedData(string $filename) : string
-    {
-        $filename = escapeshellarg($filename);
-        $output = `2>&1 gpg --list-only --verbose $filename`;
-        $lines = explode(PHP_EOL, $output);
-        $words = explode(' ', $lines[0]);
-        return end($words);
+        $this->data[$key] = $value;
+        return $this;
     }
 }
---
+++ tests/unit/DatabaseManagementSystemTest.php
@@ -0,0 +1,181 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use gpgl\core\DatabaseManagementSystem;
+
+class DatabaseManagementSystemTest extends TestCase
+{
+    protected $filename_pw = __DIR__.'/../fixtures/pw.gpgldb';
+    protected $db_pw;
+    protected $key_pw = 'jeff@example.com';
+    protected $password = 'password';
+
+    protected $filename_nopw = __DIR__.'/../fixtures/nopw.gpgldb';
+    protected $db_nopw;
+    protected $key_nopw = 'nopassword@example.com';
+
+    protected function setUp()
+    {
+        $this->db_pw = file_get_contents($this->filename_pw);
+        $this->db_nopw = file_get_contents($this->filename_nopw);
+    }
+
+    protected function tearDown()
+    {
+        file_put_contents($this->filename_pw, $this->db_pw);
+        file_put_contents($this->filename_nopw, $this->db_nopw);
+    }
+
+    public function test_instantiates_dbms_class()
+    {
+        $dbms = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+
+        $this->assertInstanceOf(DatabaseManagementSystem::class, $dbms);
+    }
+
+    public function test_gets_index()
+    {
+        $expected = [
+            'first',
+            'second',
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+        $actual = $dbms->index();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_gets_value()
+    {
+        $expected = [
+            "username" => "jeff",
+            "password" => "pass",
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+        $actual = $dbms->get('first');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_sets_value()
+    {
+        $expected = [
+            "username" => "jose",
+            "password" => "word",
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+
+        $empty = $dbms->get('test_sets_key');
+        $this->assertEmpty($empty);
+
+        $dbms->set('test_sets_key', $expected);
+
+        $actual = $dbms->get('test_sets_key');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_saves_dbms_to_file()
+    {
+        $expected = [
+            "username" => "jose",
+            "password" => "word",
+        ];
+
+        $orig = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+        $empty = $orig->get('test_saves_db');
+        $this->assertEmpty($empty);
+
+        $orig->set('test_saves_db', $expected);
+        $orig->export();
+
+        $new = new DatabaseManagementSystem($this->filename_pw, $this->password, $this->key_pw);
+        $actual = $new->get('test_saves_db');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_gets_value_without_password()
+    {
+        $expected = [
+            "username" => "none",
+            "password" => "nopw",
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_nopw, $password = null, $this->key_nopw);
+        $actual = $dbms->get('one');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @expectedException TypeError
+     */
+    public function test_rejects_missing_password()
+    {
+        $dbms = new DatabaseManagementSystem($this->filename_pw);
+        $actual = $dbms->get('first');
+    }
+
+    /**
+     * @expectedException Crypt_GPG_BadPassphraseException
+     */
+    public function test_rejects_bad_password_with_key()
+    {
+        $dbms = new DatabaseManagementSystem($this->filename_pw, 'bad password', $this->key_pw);
+        $actual = $dbms->get('first');
+    }
+
+    /**
+     * @expectedException Crypt_GPG_BadPassphraseException
+     */
+    public function test_rejects_bad_password_without_key()
+    {
+        $dbms = new DatabaseManagementSystem($this->filename_pw, 'bad password');
+        $actual = $dbms->get('first');
+    }
+
+    public function test_deduces_key_with_password()
+    {
+        $expected = [
+            "username" => "jeff",
+            "password" => "pass",
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_pw, $this->password);
+        $actual = $dbms->get('first');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_deduces_key_without_password()
+    {
+        $expected = [
+            "username" => "none",
+            "password" => "nopw",
+        ];
+
+        $dbms = new DatabaseManagementSystem($this->filename_nopw);
+        $actual = $dbms->get('one');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_creates_dbms()
+    {
+        $filename = 'test_creates_dbms.gpgldb';
+        touch($filename);
+        unlink($filename);
+        $this->assertFileNotExists($filename);
+
+        $dbms = DatabaseManagementSystem::create($filename, $this->key_nopw);
+        $this->assertFileExists($filename);
+
+        $this->assertInstanceOf(DatabaseManagementSystem::class, $dbms);
+
+        unlink($filename);
+    }
+}
