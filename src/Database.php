9270ad3ec36a5defd6658f7595c7a438a9872a7c
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-02 17:02:58 +0000

    wip create database file, skip env [ci skip]

--- src/Database.php
+++ src/Database.php
@@ -2,105 +2,29 @@
 
 namespace gpgl\core;
 
-use Crypt_GPG;
-use Crypt_GPG_BadPassphraseException;
-use InvalidArgumentException;
-
 class Database
 {
-    protected $filename;
     protected $data = [];
-    protected $gpg;
-    protected $key;
-    protected $password;
 
-    public function __construct(string $filename = null, string $password = null, string $key = null)
+    public function getData() : array
     {
-        $this->gpg = new Crypt_GPG;
-
-        $this->key = $key;
-        $this->password = $password;
-
-        if (!empty($filename)) {
-            $this->import($filename, $password, $key);
-        }
+        return $this->data;
     }
 
-    public static function create(string $filename, string $key, string $password = null) : Database
+    public function setData(array $data) : Database
     {
-        if (false === file_put_contents($filename, json_encode([]))) {
-            throw new InvalidArgumentException("Could not write: $filename");
-        }
-
-        $db = new static($filename = null, $password, $key);
-        $db->import($filename, $password = null, $key = null, $encrypted = false);
-
-        return $db;
+        $this->data = $data;
+        return $this;
     }
 
-    public function index() : array
+    public function get(string $key)
     {
-        return array_keys($this->data);
+        return $this->data[$key] ?? null;
     }
 
-    public function get(string $key) : array
+    public function set(string $key, $value) : Database
     {
-        return $this->data[$key] ?? [];
-    }
-
-    public function set(string $key, array $values) : bool
-    {
-        return $values === $this->data[$key] = $values;
-    }
-
-    public function import(string $filename, string $password = null, string $key = null, bool $encrypted = true) : array
-    {
-        if ($encrypted) {
-            try {
-
-                // default no password needed
-                $json = $this->gpg->decryptFile($filename);
-
-            } catch (Crypt_GPG_BadPassphraseException $ex) {
-
-                if (empty($key)) {
-                    $key = static::getFirstKeyIdFromEncryptedData($filename);
-                }
-
-                $json = $this->gpg
-                    ->addDecryptKey($key, $password)
-                    ->decryptFile($filename);
-
-            }
-        }
-
-        $this->filename = $filename;
-        $this->key = $key;
-        $this->password = $password;
-
-        return $this->data = json_decode($json, $array = true);
-    }
-
-    public function export() : bool
-    {
-        $json = json_encode($this->data);
-
-        $encrypted = $this->gpg->addEncryptKey($this->key)
-                        ->encrypt($json, $ascii = false);
-
-        return false !== file_put_contents($this->filename, $encrypted);
-    }
-
-    /**
-     * @param  string $filename Name of encrypted file.
-     * @return string The key ID of the *first* recipient.
-     */
-    public static function getFirstKeyIdFromEncryptedData(string $filename) : string
-    {
-        $filename = escapeshellarg($filename);
-        $output = `2>&1 gpg --list-only --verbose $filename`;
-        $lines = explode(PHP_EOL, $output);
-        $words = explode(' ', $lines[0]);
-        return end($words);
+        $this->data[$key] = $value;
+        return $this;
     }
 }
---
+++ src/DatabaseManagementSystem.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace gpgl\core;
+
+use Crypt_GPG;
+use Crypt_GPG_BadPassphraseException;
+use Exceptions\UnwritableFile;
+
+class DatabaseManagementSystem
+{
+    protected $database;
+    protected $gpg;
+    protected $filename;
+    protected $key;
+    protected $password;
+
+    public function __construct(string $filename = null, string $password = null, string $key = null)
+    {
+        $this->database = new Database;
+
+        $this->gpg = new Crypt_GPG;
+
+        if (isset($password)) {
+            $this->setPassword($password);
+        }
+
+        if (isset($key)) {
+            $this->setKey($key);
+        }
+
+        if (isset($filename)) {
+            $this->setFilename($filename)->import();
+        }
+    }
+
+    public static function create(string $filename, string $key, string $password = null) : DatabaseManagementSystem
+    {
+        $dbms = new static;
+
+        $dbms->setFilename($filename)->setKey($key);
+
+        if (isset($password)) {
+            $this->setPassword($password);
+        }
+
+        $dbms->export();
+
+        return $dbms;
+    }
+
+    public function index() : array
+    {
+        return array_keys($this->database->getData());
+    }
+
+    public function get(string $key) : array
+    {
+        return $this->database->get($key) ?? [];
+    }
+
+    public function set(string $key, array $values) : DatabaseManagementSystem
+    {
+        $this->database->set($key, $values);
+        return $this;
+    }
+
+    public function getFilename() : string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename) : DatabaseManagementSystem
+    {
+        $this->filename = $filename;
+        return $this;
+    }
+
+    public function getKey() : string
+    {
+        if (empty($this->key)) {
+            $this->key = static::getFirstKeyIdFromEncryptedData($this->getFilename());
+        }
+
+        return $this->key;
+    }
+
+    public function setKey(string $key) : DatabaseManagementSystem
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    protected function getPassword() : string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password) : DatabaseManagementSystem
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function import() : DatabaseManagementSystem
+    {
+        try {
+
+            // default no password needed
+            $json = $this->gpg->decryptFile($this->getFilename());
+
+        } catch (Crypt_GPG_BadPassphraseException $ex) {
+
+            $json = $this->gpg
+                ->addDecryptKey($this->getKey(), $this->getPassword())
+                ->decryptFile($this->getFilename());
+
+        }
+
+        $data = json_decode($json, $array = true);
+
+        $this->database->setData($data);
+
+        return $this;
+    }
+
+    public function export() : DatabaseManagementSystem
+    {
+        $json = json_encode($this->database->getData());
+
+        $encryptedData = $this->gpg
+            ->addEncryptKey($this->getKey())
+            ->encrypt($json, $ascii = false);
+
+        $filename = $this->getFilename();
+
+        if (false === file_put_contents($filename, $encryptedData)) {
+            throw new UnwritableFile("Could not write file: $filename");
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param  string $filename Name of encrypted file.
+     * @return string The key ID of the *first* recipient.
+     */
+    public static function getFirstKeyIdFromEncryptedData(string $filename) : string
+    {
+        $filename = escapeshellarg($filename);
+        $output = `2>&1 gpg --list-only --verbose $filename`;
+        $lines = explode(PHP_EOL, $output);
+        $words = explode(' ', $lines[0]);
+        return end($words);
+    }
+}
