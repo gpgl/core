8764eb2807719d6a13bc9043e44dc79dd7d66cd8
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-17 14:33:59 +0000

    validate array/json input

--- src/History.php
+++ src/History.php
@@ -2,12 +2,23 @@
 
 namespace gpgl\core;
 
+use gpgl\core\Exceptions\InvalidHistoryChain;
+
 class History
 {
     protected $chain = [];
 
     public function __construct($chain)
     {
+        if (is_string($chain)) {
+            $chain = json_decode($chain, $array = true);
+            $error = 'JSON decode: '.json_last_error_msg();
+        }
+
+        if (!is_array($chain)) {
+            throw new InvalidHistoryChain($error ?? null);
+        }
+
         $this->chain = $chain;
     }
 }
